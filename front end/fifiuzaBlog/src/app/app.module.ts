a699d713173fbfe4ec1de328b3761d5e6126fea9
Maicon Fiuza <maicon.b.fiuza@gmail.com>
2021-05-26 20:50:55 -0300

    adicionado o menu

--- front end/fifiuzaBlog/src/app/app.module.ts
+++ front end/fifiuzaBlog/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
