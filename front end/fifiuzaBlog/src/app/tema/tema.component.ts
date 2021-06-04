d96fa4b1483d48578cd8c237eb15d16e16a758b2
Maicon Fiuza <maicon.b.fiuza@gmail.com>
2021-06-04 10:45:15 -0300

    Método get e post tema

--- front end/fifiuzaBlog/src/app/tema/tema.component.ts
+++ front end/fifiuzaBlog/src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
+import { TemaService } from './../service/tema.service';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
+import { Tema } from '../model/Tema';
 
 @Component({
   selector: 'app-tema',
@@ -9,11 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
+  constructor(private router: Router,
+              private temaService:TemaService
+    ) { }
 
   ngOnInit(){
     if(environment.token ==''){
        this.router.navigate([('/login')])
     }
+
+    this.findAllTema()
+
+  }
+
+  findAllTema(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp:Tema)=>{
+      this.tema=resp
+      alert('Tema cadastrado com sucesso')
+      this.findAllTema()
+      this.tema = new Tema()
+    })
   }
 }
